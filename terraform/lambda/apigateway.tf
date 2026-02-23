7916f1ffdde0f992fdcf11c3dbcfeac2bad751ae
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-23 20:48:18 -0300

    create CD to lambda

--- terraform/lambda/apigateway.tf
+++ terraform/lambda/apigateway.tf
@@ -1,83 +1,21 @@
-resource "aws_api_gateway_rest_api" "api" {
-  name = "my-rest-api"
-}
+module "api_gateway" {
+  source  = "terraform-aws-modules/apigateway-v2/aws" # Exemplo para HTTP API, mais simples e barato
+  name    = "my-fastapi"
+  protocol_type = "HTTP"
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "root_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "root_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_rest_api.api.root_resource_id
-  http_method             = aws_api_gateway_method.root_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.docker_lambda.invoke_arn
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.docker_lambda.invoke_arn
-}
-
-
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.docker_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_stage" "stage" {
-  stage_name    = "prod"
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  deployment_id = aws_api_gateway_deployment.deployment.id
-}
-
-# ... (seus recursos de api, resource e methods continuam iguais)
-
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-
-  # Isso força o redeploy se houver qualquer mudança nos recursos acima
-  triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.resource.id,
-      aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.integration.id,
-      aws_api_gateway_method.root_method.id,
-      aws_api_gateway_integration.root_integration.id,
-    ]))
+  cors_configuration = {
+    allow_origins = ["*"]
   }
 
-  lifecycle {
-    create_before_destroy = true
+  # Integração direta com o Lambda
+  integrations = {
+    "ANY /{proxy+}" = {
+      lambda_arn             = aws_lambda_function.docker_lambda.arn
+      payload_format_version = "2.0"
+    }
+    "ANY /" = {
+      lambda_arn             = aws_lambda_function.docker_lambda.arn
+      payload_format_version = "2.0"
+    }
   }
-
-  depends_on = [
-    aws_api_gateway_integration.integration,
-    aws_api_gateway_integration.root_integration
-  ]
 }
