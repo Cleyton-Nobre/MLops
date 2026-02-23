e35ae068630433aad325450bba0e189d27484a02
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-23 20:10:26 -0300

    create CD to lambda

--- terraform/lambda/apigateway.tf
+++ terraform/lambda/apigateway.tf
@@ -1,31 +1,47 @@
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "lambda-http-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "api" {
+  name = "my-rest-api"
 }
 
-resource "aws_lambda_permission" "apí_gateway" {
-  statement_id = "AllowAPIGatewayInvoke"
-  action    = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.docker_lambda.function_name
-  principal ="apigateway.amazonaws.com"
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "invoke"
 }
 
-resource "aws_apigatewayv2_integration" "lambda" {
-  api_id = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri = aws_lambda_function.docker_lambda.invoke_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_route" "route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app.invoke_arn
 }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.app.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  depends_on = [
+    aws_api_gateway_integration.integration
+  ]
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
 }
