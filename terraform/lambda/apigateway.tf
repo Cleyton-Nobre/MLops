c54c8c829abf141f406df9af0fa8b765a90167bf
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-23 20:58:05 -0300

    create CD to lambda

--- terraform/lambda/apigateway.tf
+++ terraform/lambda/apigateway.tf
@@ -7,7 +7,7 @@ resource "aws_apigatewayv2_api" "http_api" {
 # 2. Cria o Stage (O $default faz com que não precise de /prod na URL)
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
+  name  = "prod"
   auto_deploy = true
 }
 
