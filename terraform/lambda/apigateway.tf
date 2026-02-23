cd7288fd1a76c487aab2f9cd73a5b9c7fdd42948
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-23 20:20:17 -0300

    create CD to lambda

--- terraform/lambda/apigateway.tf
+++ terraform/lambda/apigateway.tf
@@ -5,13 +5,13 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "invoke"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
