54bc38694a2c43583bef5d61e9d2c46da565097b
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-23 21:09:05 -0300

    Revert "create CD to lambda"
    This reverts commit cbc2e88e69f5c67a483de17cf77397ff2d2cfe44.

--- terraform/lambda/apigateway.tf
+++ terraform/lambda/apigateway.tf
@@ -1,37 +1,21 @@
 module "api_gateway" {
-  source  = "terraform-aws-modules/apigateway-v2/aws"
-  version = "~> 5.0" # Garante a versão mais estável
-
-  name          = "my-fastapi-http"
-  description   = "API Gateway v2 para FastAPI"
+  source  = "terraform-aws-modules/apigateway-v2/aws" # Exemplo para HTTP API, mais simples e barato
+  name    = "my-fastapi"
   protocol_type = "HTTP"
 
-  create_api_domain_name = false # Coloque true se tiver um domínio próprio
+  cors_configuration = {
+    allow_origins = ["*"]
+  }
 
-  # O segredo está aqui: uma única integração para tudo
+  # Integração direta com o Lambda
   integrations = {
     "ANY /{proxy+}" = {
       lambda_arn             = aws_lambda_function.docker_lambda.arn
       payload_format_version = "2.0"
-      timeout_milliseconds   = 30000
     }
     "ANY /" = {
       lambda_arn             = aws_lambda_function.docker_lambda.arn
       payload_format_version = "2.0"
-      timeout_milliseconds   = 30000
     }
   }
-
-  tags = {
-    Environment = "prod"
-  }
-}
-
-# Permissão para o API Gateway chamar o Lambda (necessário mesmo com módulo)
-resource "aws_lambda_permission" "apigw_v2" {
-  statement_id  = "AllowExecutionFromAPIGatewayV2"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.docker_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${module.api_gateway.apigatewayv2_api_execution_arn}/*/*"
 }
