b58700fd543d95b0700e1a8eac59a044a5226773
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-26 08:44:51 -0300

    fix lambda to s3

--- app/train_model.py
+++ app/train_model.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import joblib
+import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import recall_score, accuracy_score, roc_auc_score
+from sklearn.metrics import recall_score, accuracy_score, roc_auc_score, precision_score, roc_curve
 
 def train_model(data):
     data = pd.read_csv(data)
@@ -22,14 +23,30 @@ def train_model(data):
     recall = recall_score(y_test, y_pred)
     prob=models.predict_proba(X_test)[:, 1]
     auc = roc_auc_score(y_test, prob)
+    precision = precision_score(y_test, y_pred)
 
     joblib.dump(models, "model.pkl")
 
-    return {
-        "accuracy": accuracy,
-        "recall": recall,
-        "auc": auc
-    }
+    with open("metrics.txt", "w") as f:
+        f.write(f"Acurácia: {accuracy}\nRecall: {recall}\nAUC: {auc} \n Precision: {precision}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot()
+
+    # 2. Criar o plot
+    fpr, tpr, _ = roc_curve(y_test, prob)
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+
+    # 3. Salvar como imagem para o CML ler
+    plt.savefig("auc_plot.png")
 
 if __name__ == "__main__":
     metrics = train_model("../data/music_clean.csv")
