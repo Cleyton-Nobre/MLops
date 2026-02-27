14c7d3310ffd750bad3f2c2d8b5f8235b0c14c70
Cleyton Nobre <cleyton.nobre@lacreisaude.com.br>
2026-02-27 08:30:04 -0300

    create role cloud watch

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -25,6 +25,28 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "lambda_custom_metrics" {
+  name = "lambda-custom-metrics"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_custom_metrics" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_custom_metrics.arn
+}
+
 # Permissões básicas (CloudWatch Logs)
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
@@ -45,5 +67,5 @@ resource "aws_lambda_function" "docker_lambda" {
   timeout     = 30
   memory_size = 512
 
-  depends_on = [aws_iam_role_policy_attachment.lambda_logs]
+  depends_on = [aws_iam_role_policy_attachment.lambda_logs, aws_iam_role_policy_attachment.attach_custom_metrics]
 }
